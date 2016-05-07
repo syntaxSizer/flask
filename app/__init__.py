bbc87ad3c9beb22b22593170bb62681da5023adf
syntaxSizer <kakuma4never@gmail.com>
2016-05-07 20:55:33 +0300

    initilize Database

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+#we are creating DB object 'db' that will be our DB
+db = SQLAlchemy(app)
 
-from app import views
+from app import views, models
