682351588ae40ea85e7a82dfeee3fc2c38158692
syntaxSizer <kakuma4never@gmail.com>
2016-05-08 15:24:01 +0300

    fix typo error

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class post(db.Model):
       id = db.Column(db.Integer,primary_key = True)
       body = db.Column(db.String(140))
       timestamp = db.Column(db.DateTime)
-      user_id = db.Column(db.Integer, db.ForeignKey('user_id'))
+      user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
       
  
       def __repr__(self):
