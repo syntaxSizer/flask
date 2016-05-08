4b7558732f25507e6a79cbdfce03c991e8dd90d0
syntaxSizer <kakuma4never@gmail.com>
2016-05-08 13:42:14 +0300

    add database upgrade and downgrade scripts

--- app/models.py
+++ app/models.py
@@ -4,6 +4,19 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    posts = db.relationship('post',backref='author', lazy='dynamic')
+
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
+
+class post(db.Model1):
+      id = db.Column(db.Integer,primary_key = True)
+      body = db.Column(db.String(140))
+      timestamp = db.Column(db.DateTime)
+      user_id = Ddb.Column(db.Integer, db.ForeignKey('user_id'))
+      
+ 
+      def __repr__(self):
+          return '<post %r>' % (self.body)
