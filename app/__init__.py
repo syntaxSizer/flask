cbd8c43c9bc7c55c55635260fe94a0b62d280002
syntaxSizer <kakuma4never@gmail.com>
2016-05-09 18:54:30 +0300

    openID login callback configration

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,21 @@
+import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-from app import views, models
+from app import views, models, app
+
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
--- app/models.py
+++ app/models.py
@@ -5,6 +5,28 @@ class User(db.Model):
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('post',backref='author', lazy='dynamic')
+   
+    @property
+    def is_authenticated(self):
+        return True
+   
+     @property
+    def is_active(self):
+        return True
+      
+      @property
+    def is_anonymous(self):
+          return False
+
+      @property
+    def get_id(self):
+        try:
+            return unicode(self.id) # python 2
+        except NameError:
+            return str(self.id) #python 3
+        
+    def __repr__(self):
+           return '<User %r>' (self.nickname)
 
 
     def __repr__(self):
@@ -18,5 +40,5 @@ class post(db.Model):
       user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
       
  
-      def __repr__(self):
-          return '<post %r>' % (self.body)
+    def __repr__(self):
+        return '<post %r>' % (self.body)
